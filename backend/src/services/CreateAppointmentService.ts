4010c753ac994539701e8afabebe149c80ee75f1
Renan Felipe <renan.fb.dantas@gmail.com>
2020-04-26 22:18:42 -0300

    feat: add user verification on appointment create

--- backend/src/services/CreateAppointmentService.ts
+++ backend/src/services/CreateAppointmentService.ts
@@ -3,6 +3,7 @@ import { getCustomRepository } from 'typeorm';
 
 import Appointment from '../models/Appointment';
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
+import UsersRepository from '../repositories/UsersRepository';
 
 interface Request {
   provider_id: string;
@@ -12,12 +13,23 @@ interface Request {
 class CreateAppointmentService {
   public async execute({ provider_id, date }: Request): Promise<Appointment> {
     const appointmentsRepository = getCustomRepository(AppointmentsRepository);
+    const usersRepository = getCustomRepository(UsersRepository);
 
     const appointmentDate = startOfHour(date);
 
     const findAppointmentInSameDate = await appointmentsRepository.findByDate(
       appointmentDate,
     );
+
+    const userExists = await usersRepository.findOne({
+      where: {
+        id: provider_id,
+      },
+    });
+
+    if (!userExists) {
+      throw new Error('User not found.');
+    }
 
     if (findAppointmentInSameDate) {
       throw Error('This appointment is already booked.');
