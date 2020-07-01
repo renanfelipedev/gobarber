b6a8d84b40be72ee2d2778b1b520ea9c840fceb3
Renan Felipe <renan.fb.dantas@gmail.com>
2020-06-30 23:22:30 -0300

    refact: add mail template provider on all mail providers

--- backend/src/shared/container/providers/MailProvider/implementations/EtherealMailProvider.ts
+++ backend/src/shared/container/providers/MailProvider/implementations/EtherealMailProvider.ts
@@ -1,11 +1,18 @@
+import { injectable, inject } from 'tsyringe';
 import nodemailer, { Transporter } from 'nodemailer';
 
+import IMailTemplateProvider from '@shared/container/providers/MailTemplateProvider/models/IMailTemplateProvider';
 import IMailProvider from '../models/IMailProvider';
+import ISendMailDTO from '../dtos/ISendMailDTO';
 
-export default class EtherealMailProvider implements IMailProvider {
+@injectable()
+class EtherealMailProvider implements IMailProvider {
   private client: Transporter;
 
-  constructor() {
+  constructor(
+    @inject('MailTemplateProvider')
+    private mailTemplateProvider: IMailTemplateProvider,
+  ) {
     nodemailer.createTestAccount().then(account => {
       const transporter = nodemailer.createTransport({
         host: 'smtp.ethereal.email',
@@ -21,16 +28,28 @@ export default class EtherealMailProvider implements IMailProvider {
     });
   }
 
-  public async sendMail(to: string, body: string): Promise<void> {
+  public async sendMail({
+    to,
+    subject,
+    from,
+    templateData,
+  }: ISendMailDTO): Promise<void> {
     const message = await this.client.sendMail({
-      from: 'Equipe GoBarber <equipe@gobarber.com.br>',
-      to,
-      subject: '',
-      text: body,
-      // html: '<p><b>Hello</b> to myself!</p>',
+      from: {
+        name: from?.name || 'Equipe GoBarber',
+        address: from?.email || 'equipe@gobarber.com.br',
+      },
+      to: {
+        name: to.name,
+        address: to.email,
+      },
+      subject,
+      html: await this.mailTemplateProvider.parse(templateData),
     });
 
-    console.log('Mensagem enviada: %s', message.messageId);
+    console.log('Mensagem: %s', message.messageId);
     console.log('URL: %s', nodemailer.getTestMessageUrl(message));
   }
 }
+
+export default EtherealMailProvider;
